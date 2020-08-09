e1d08603e44eb66b24030e6ab5373e34bfe41731
DesmondOPe <desmond.bai94@outlook.com>
2020-08-09 23:59:02 +0800

    *Add initial enemy path movement   *Add Text Mesh to Block to indicate current location

---
+++ Realm_Rush/Assets/CubeEditor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[ExecuteInEditMode]
+[SelectionBase] //Select Entire Cube When Clicked
+public class CubeEditor : MonoBehaviour
+{
+  
+    [SerializeField] [Range(1f, 20f)] float gridSize = 10f;
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 snapPos;
+        snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
+        snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+        snapPos.y = 0f;
+
+        transform.position = snapPos;
+
+        TextMesh textMesh = GetComponentInChildren<TextMesh>();
+
+        string coord = "(" + (snapPos.x / gridSize).ToString() + "," + (snapPos.z / gridSize).ToString() + ")";
+
+        textMesh.text = coord;
+        gameObject.name = coord;
+    }
+}
---
+++ Realm_Rush/Assets/EnemyMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] List<Waypoint> pathList;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(FollowPath());
+        print("Unity is taking Control!!!!!");
+    }
+
+    IEnumerator FollowPath()
+    {
+        print("Start Patrol!");
+        foreach (Waypoint waypoint in pathList)
+        {
+            transform.position = waypoint.transform.position;
+            yield return new WaitForSeconds(1f);
+            print("Visiting Block" + waypoint.name + "!");
+        }
+
+        print("End Patrol!");
+    }
+
+    // Update is called once per frame
+  
+}
