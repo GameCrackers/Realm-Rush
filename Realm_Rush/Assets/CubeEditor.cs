09bdad6af78cbc04dcd61e5bfb35802b1363df7f
DesmondOPe <desmond.bai94@outlook.com>
2020-08-12 23:37:25 +0800

    Refactor Code; Redefine Dependency Between: *CubeEditor.cs *Waypoint.cs *Eneny.cs

--- Realm_Rush/Assets/CubeEditor.cs
+++ Realm_Rush/Assets/CubeEditor.cs
@@ -5,25 +5,52 @@ using UnityEngine;
 
 [ExecuteInEditMode]
 [SelectionBase] //Select Entire Cube When Clicked
+[RequireComponent(typeof(Waypoint))]
 public class CubeEditor : MonoBehaviour
 {
-  
-    [SerializeField] [Range(1f, 20f)] float gridSize = 10f;
+    Waypoint waypoint;
+    //   Vector3 gridPos;
+
+    //[SerializeField] [Range(1f, 20f)] float gridSize = 10f;
+    private void Awake()
+    {
+        waypoint = GetComponent<Waypoint>();
+    }
+
     // Update is called once per frame
     void Update()
     {
-        Vector3 snapPos;
-        snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
-        snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
-        snapPos.y = 0f;
 
-        transform.position = snapPos;
+        int gridSize = waypoint.GetGridSize();
 
+        Vector3 gridPos = new Vector3(
+            waypoint.GetPosition().x,
+            0f,
+            waypoint.GetPosition().y
+            );
+
+        SnapToPosition(gridSize, gridPos);
+
+        UpdateLabel(gridSize, gridPos);
+    }
+
+    private void UpdateLabel(int gridSize, Vector3 gridPos)
+    {
         TextMesh textMesh = GetComponentInChildren<TextMesh>();
 
-        string coord = "(" + (snapPos.x / gridSize).ToString() + "," + (snapPos.z / gridSize).ToString() + ")";
+        string coord = 
+            "(" + (gridPos.x).ToString() + 
+            "," + 
+            (gridPos.z).ToString() + ")";
 
         textMesh.text = coord;
         gameObject.name = coord;
+    }
+
+    private void SnapToPosition(int gridSize, Vector3 gridPos)
+    {
+
+
+        transform.position = gridPos * gridSize;
     }
 }
