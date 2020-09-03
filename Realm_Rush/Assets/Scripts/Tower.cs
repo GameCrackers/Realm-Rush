cda1037f3b2dd5bd14610a41d92a946d56a416af
DesmondOPe <desmond.bai94@outlook.com>
2020-09-03 23:41:17 +0800

    *Add Tower that looks at enenmy   *Add particle system of tower bullets   *Enable collision detection on enemy

---
+++ Realm_Rush/Assets/Scripts/Tower.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    [SerializeField] Transform objectToMove;
+    [SerializeField] Transform targetToLookAt;
+    // Start is called before the first frame update
+    // Update is called once per frame
+    void Update()
+    {
+        objectToMove.LookAt(targetToLookAt);
+    }
+}
